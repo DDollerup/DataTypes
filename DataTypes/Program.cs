0ac4ef4a36fd6c7815ef5244fafd0b275c0d35a4
PDDaniel <daniel@proper-dog.com>
2017-11-21 12:25:07 +0100

    Add project files.

---
+++ DataTypes/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypes
+{
+    class DanielsTank
+    {
+        string tankName = "Daniels Tank";
+        int health = 1000;
+        bool alive = true;
+
+        void Main(string[] args)
+        {
+
+            if (health <= 0)
+            {
+
+            }
+
+            if (health >= 500 && health <= 750)
+            {
+
+            }
+
+            if (health < 200 || health > 100)
+            {
+
+            }
+
+
+            if (health <= 0)
+            {
+                // Dead
+            }
+            else if (health > 0 && health < 500)
+            {
+                // Print health as Red
+            }
+            else
+            {
+                // Alive
+            }
+
+            if (LessThanHundrethHealth())
+            {
+
+            }
+
+
+
+        }
+
+        void Shoot()
+        {
+            // Få tanken til at skyde
+        }
+
+        bool LessThanHundrethHealth()
+        {
+            if (health < 100)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        void HealthPickup()
+        {
+            health += 100;
+            if (health > 1000)
+            {
+                health = 1000;
+            }
+        }
+
+        void TakeDamage(int amount)
+        {
+            health -= amount;
+            if (health <= 0)
+            {
+                alive = false;
+            }
+        }
+
+        void OnBeingHit()
+        {
+            TakeDamage(33);
+        }
+    }
+}
